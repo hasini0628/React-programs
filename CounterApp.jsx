d09a6cac2d638b2673908316b1fbf2361bc40941
Hasini <hasinielikatti06@gmail.com>
2026-08-13 15:38:47 +0530

    Add increase function to update count and clicks

--- CounterApp.jsx
+++ CounterApp.jsx
@@ -3,3 +3,7 @@ function App(){
   const [count, setCount] = useState(0);
   const [clicks, setClicks] = useState(0);
   const [decreaseClicks, setDecreaseClicks] = useState(0);
+  function increase() {
+    setCount(count + 1);
+    setClicks(clicks +1);
+  }
