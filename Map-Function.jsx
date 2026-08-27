9a4ead90f6f936a6f670b08caadc357fd7f2a7e0
Hasini <hasinielikatti06@gmail.com>
2026-08-27 11:19:34 +0530

    Implement map function to display subjects

--- Map-Function.jsx
+++ Map-Function.jsx
@@ -6,3 +6,13 @@ function App(){
   "HTML",
   "CSS"
 ];
+   return(
+    <div> 
+      <h1>My favorite subjects</h1>
+      {subjects.map((subject,index)=>(
+        <h2 key={index}>{subject}</h2>
+      ))}
+    </div> 
+  )
+};
+export default App;
