ea1406dc21ea596173aa699145c6f5efd1c129e7
Hasini <hasinielikatti06@gmail.com>
2026-08-28 23:40:18 +0530

    Add useState hook for managing name and age

--- UseState.jsx
+++ UseState.jsx
@@ -1,1 +1,7 @@
-
+import { useState } from "react";
+function App(){
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  function nameChange(event){
+    setName(event.target.value);
+  }
