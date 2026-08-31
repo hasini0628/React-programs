3045c50307b67b8b0a659a8741d836dd9fdc9d7a
Hasini <hasinielikatti06@gmail.com>
2026-08-31 22:16:38 +0530

    Add return statement with profile display in UseState

--- UseState.jsx
+++ UseState.jsx
@@ -8,3 +8,9 @@ function App(){
    function ageChange(event){
     setAge(event.target.value);
   }
+ return(
+    <div>
+      <h1>My profile</h1>
+      <h1>Name:</h1>
+      <input type="text" value={name}onChange={nameChange}/>
+      <h1>Age</h1>
