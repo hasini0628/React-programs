c5241f224bf5cc22dc8bb7b0ab5bfb3b9aa9137f
Hasini <hasinielikatti06@gmail.com>
2026-09-09 11:25:24 +0530

    Add state management for email and password inputs

--- Login-form.jsx
+++ Login-form.jsx
@@ -1,1 +1,8 @@
-
+import { useState } from "react";
+function App(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  function emailChange(event){
+    setEmail(event.target.value);
+  }
